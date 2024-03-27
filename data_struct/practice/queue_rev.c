aa97d46d4f6b7367d55bea7ce593a123c553ba35
Brooke Joseph <114883803+brookejoseph@users.noreply.github.com>
2024-03-27 09:48:12 -0400

    Create queue_rev.c

---
+++ data_struct/practice/queue_rev.c
@@ -0,0 +1,32 @@
+struct queue *mine{
+    int len;
+    int *data;
+}
+
+int front(struct queue *q){
+    return q->data[0];
+}
+
+void enqueue(struct queue *q, int val){
+    q->len++;
+    q->data = malloc(sizeof(int) * (q->len));
+    assert(q->data);
+    q->data[q->len - 1] = val;
+}
+
+void dequeue(struct queue *q){
+    for(int i = 1; i < q->len; i++){
+        q->data[i - 1] = q->data[i];
+    }
+    q->len--;
+    q->data = realloc(q->data, sizeof(struct queue) * q->len);
+}
+
+struct queue rev(struct queue *q){
+    dequeue(q);
+    for(int i = 0; i < q->len; i++){
+        enqueue(q, q->data[i]);
+    }
+
+    return q;
+}
