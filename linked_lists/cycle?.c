1e740faa71a0f9c0b44d66c17f0788714ec1106f
Brooke Joseph <114883803+brookejoseph@users.noreply.github.com>
2024-03-29 15:20:48 -0400

    Update cycle?.c

--- linked_lists/cycle?.c
+++ linked_lists/cycle?.c
@@ -9,8 +9,23 @@ struct llist {
 
 bool cycle(struct llist *m){
   struct llnode *current = m->front;
-  struct llnode **list = current->
+  int cap = 10; 
+  int size = 0;
+  struct llnode **list = malloc(sizeof(struct llnode) * cap);
   while(current->next != NULL){
-    
+    if(cap == size){
+      cap *= 2;
+    }
+    list[size] = current;
+    size++;
+    current = current->next;
+  }
+  for(int i = 0; i < size; i++){
+    for(int l = i + 1; l < size; l++){
+      if(list[l] == list[i]){
+        return true;
+      }
+      return false;
+    }
   }
 }
