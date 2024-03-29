5d18f63dcb49bdbd16c301c328eea57c75f0b632
Brooke Joseph <114883803+brookejoseph@users.noreply.github.com>
2024-03-29 15:09:32 -0400

    Create cycle?.c

---
+++ linked_lists/cycle?.c
@@ -0,0 +1,16 @@
+struct llnode {
+  int data; 
+  struct llnode *next; 
+};
+
+struct llist {
+  struct llnode *front; 
+};
+
+bool cycle(struct llist *m){
+  struct llnode *current = m->front;
+  struct llnode **list = current->
+  while(current->next != NULL){
+    
+  }
+}
