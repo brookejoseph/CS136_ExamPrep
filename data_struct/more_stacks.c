400dcabe5b72fa58cfe7f58188b84014c079dab8
Brooke Joseph <114883803+brookejoseph@users.noreply.github.com>
2024-04-17 13:59:13 -0400

    Update more_stacks.c

--- data_struct/more_stacks.c
+++ data_struct/more_stacks.c
@@ -30,3 +30,59 @@ int top(struct my_stack *m){
 }
 
 //linked lists 
+struct llnode{
+    int val;
+    struct llnode *next;
+};
+
+struct my_stack{
+    struct llnode *front;
+}
+
+
+struct my_stack *create(void){
+    struct my_stack *mine = malloc(sizeof(struct my_stack));
+    mine->front = NULL;
+    return mine;
+}
+
+struct llnode *create_llnode(int val){
+    struct llnode *mine = malloc(sizeof(struct llnode));
+    mine->val = val;
+    mine->next = NULL;
+    return mine;
+}
+
+void destory(struct my_stack *mine){
+    struct llnode *current = mine->front;
+    struct llnode *prev = NULL
+    while(current != NULL){
+        prev = current;
+        free(prev);
+        current = current->next;
+    }
+    free(mine);
+}
+
+void push(struct my_stack *m, int val){
+    struct llnode *new = create_llnode(val);
+    struct llnode *current = m->front;
+    struct llnode prev = NULL;
+    while(current != NULL){
+        prev = current;
+        current = current->next;
+    }
+    prev->next = new;
+}
+
+int top(struct my_stack *m){
+    struct llnode *current = m->front;
+    struct llnode prev = NULL;
+    while(current != NULL){
+        prev = current;
+        current = current->next;
+    }
+    return prev->val;
+}
+
+//
